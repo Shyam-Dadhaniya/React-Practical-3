30d3111e9fa6e16fe8d13910361d0ab833480197
Shyam dadhaniya <shyam.dadhaniya04@gmail.com>
2022-03-11 17:23:01 +0530

    Update

--- src/styled/ToDo/NotaskStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-const NotaskStyle = styled.div`
-  text-align: center;
-  color: grey;
-  margin-top: 110px;
-  font-weight: 600;
-`;
-
-export default NotaskStyle;
